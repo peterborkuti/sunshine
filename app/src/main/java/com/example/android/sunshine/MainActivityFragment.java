ec5dc27f8e7ea13d1d749acb5e626806d866cd7a
Peter Borkuti <peter.borkuti@liferay.com>
2015-12-12 15:29:04 +0100

    halfway. The next is Initialize Adapter

--- app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +20,12 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] list = {
+                "Yesterday - Sunny 9/11", "Today - Sunny 3/7","Wed - Windy 5/11",
+                "Thu - mmm 9/6", "Thu - mmm 9/6", "Thu - mmm 9/6"
+        };
+
+        ArrayAdapter
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
