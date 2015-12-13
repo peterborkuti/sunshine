4f07e3964fcfc8e00d62fb9f17249b97091e343d
Peter Borkuti <peter@debian>
2015-12-13 09:37:05 +0100

    list with text and image

--- app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,12 +22,24 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         String[] list = {
-                "Yesterday - Sunny 9/11", "Today - Sunny 3/7","Wed - Windy 5/11",
-                "Thu - mmm 9/6", "Thu - mmm 9/6", "Thu - mmm 9/6"
+                "Yesterday - Sunny 9/11 Sárika", "Today - Sunny 3/7  Sárika","Wed - Windy 5/11  Sárika",
+                "Thu - mmm 9/6", "Thu - mmm 9/6", "Thu - mmm 9/6", " Sárika",  "Sárika"
         };
 
-        ArrayAdapter
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(list));
+
+        ArrayAdapter arrAdapter =
+                new ArrayAdapter(getActivity(),R.layout.list_item_forecast,
+                        R.id.list_item_forecast_textview, weekForecast);
+
+        //Need root view for finding ListView
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+
+        listView.setAdapter(arrAdapter);
+
+        return rootView;
     }
 }
