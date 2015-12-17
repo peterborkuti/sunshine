597bfe4d6ac9bae1fd512843649c92c10015f9a4
Peter Borkuti <peter@debian>
2015-12-17 07:52:52 +0100

    options menu - not working - methods are not calling in fragmentactivity

--- app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
@@ -2,7 +2,11 @@ package com.example.android.sunshine;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -16,6 +20,9 @@ import java.util.Arrays;
  */
 public class MainActivityFragment extends Fragment {
 
+    public static final String TAG = "BP";
+    LayoutInflater inflater;
+
     public MainActivityFragment() {
     }
 
@@ -23,6 +30,7 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        this.inflater = inflater;
         String[] list = {
                 "Yesterday - Sunny 9/11 Sárika", "Today - Sunny 3/7  Sárika","Wed - Windy 5/11  Sárika",
                 "Thu - mmm 9/6", "Thu - mmm 9/6", "Thu - mmm 9/6", " Sárika",  "Sárika"
@@ -41,5 +49,29 @@ public class MainActivityFragment extends Fragment {
         listView.setAdapter(arrAdapter);
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        Log.d(TAG, "inlfating forecastfragment menu");
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh)
+        {
+            Log.d(TAG, "Refresh menu selected");
+            return true;
+        }
+
+        Log.d(TAG, "Other menu selected");
+        return super.onOptionsItemSelected(item);
     }
 }
